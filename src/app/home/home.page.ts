9b201209163bf08b9d910c120e69882e4a35c3ae
athumma79 <apoorva.thumma79@gmail.com>
2019-05-28 12:26:21 +0100

    done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  public myDateOfBirth: String;
 
-  constructor() {}
+  ionViewDidEnter() {
+    this.myDateOfBirth = "1999-29-04"
+  }
+  
+  async pressMe() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'Hi!!',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Toast',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  constructor(public toastController: ToastController, public alertController: AlertController) {}
 }
